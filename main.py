16b07222ebdb7ebd047247d517c02c764cdd732e
Dake78 <125260386+Dake78@users.noreply.github.com>
2023-05-22 23:09:55 +0200

    Update main.py

--- main.py
+++ main.py
@@ -12,7 +12,7 @@ def main():
         quotes = load_quotes("quotes.txt")
         menu()
 
-        choice = input("Choose your an action (1-3): ")
+        choice = input("Choose your an action (1-4): ")
         
         if choice == "1":
             print_quote(random_quote(quotes))
